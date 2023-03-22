56759d29e29a1529a1c8ce46566577b19936e55a
Ghidella <raphaelfghidella@gmail.com>
2023-03-22 17:47:20 -0300

    valid parentheses c++

---
+++ LeetCode/20 Valid Parentheses.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+public:
+    bool isValid(std::string s) {
+    std::stack<char> st;
+
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } else {
+            if (st.empty()) {
+                return false;
+            }
+            char top = st.top();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return false;
+            }
+            st.pop();
+        }
+    }
+
+    return st.empty();
+}
+};
