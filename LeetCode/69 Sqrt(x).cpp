c7831039f1fe667e89e0b51e80e661824312e21d
Ghidella <raphaelfghidella@gmail.com>
2023-03-28 08:39:39 -0300

    optimized solution

--- LeetCode/69 Sqrt(x).cpp
+++ LeetCode/69 Sqrt(x).cpp
@@ -1,5 +1,29 @@
 //https://leetcode.com/problems/sqrtx/
 
+
+//optimized solution with binary search O(log(x))
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x > 0 && x < 2) return 1;
+        long int left = 1, right = x/2, sol = 0;
+        while(left <= right){
+            long int mid = left + (right-left)/2;
+            if(mid <= x/mid){
+                sol = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return sol;
+    }
+};
+
+
+
+
+//first solution O(sqrt(x))
 class Solution {
 public:
     int mySqrt(int x) {
