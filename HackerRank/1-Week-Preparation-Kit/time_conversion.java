a25e8cedbe03252ced5b5a0f791c3e8c12d6afb4
Ghidella <raphaelfghidella@gmail.com>
2024-03-11 11:22:44 -0300

    simple solving

---
+++ HackerRank/1-Week-Preparation-Kit/time_conversion.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+    String hour = s.substring(0, 2);
+    int h = Integer.parseInt(hour);
+    if(s.charAt(8) == 'P'){  
+        if(h != 12) h += 12;  
+    } else {
+        if(h == 12) h = 0;
+    }
+    
+    String conv_hour = String.format("%02d", h);
+    String minSec = s.substring (2, 8);
+    return conv_hour + minSec;
+    }
+    
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
