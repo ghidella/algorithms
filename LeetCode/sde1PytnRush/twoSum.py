dc8999eedd6b1622bde6c587521d7cfea0259041
unknown <raphaelfghidella@gmail.com>
2024-12-08 14:53:54 -0300

    twoSum python solution

--- LeetCode/sde1PytnRush/twoSum.py
+++ LeetCode/sde1PytnRush/twoSum.py
@@ -3,14 +3,11 @@
 # First solution O(n^2) time and O(1) space complexity
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = []
-        for i in range(len(nums):
+        for i in range(len(nums)):
             for j in range(len(nums)):
-                if nums[i]+nums[j] == target:
-                    result[0] = i
-                    result[1] = j
+                if nums[i] + nums[j] == target and i != j:
+                    return [i, j]
                     
-        return result
         
 # Optimized solution O(n) time and space complexity
 class Solution:
